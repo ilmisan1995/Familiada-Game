5612dc4cfa3d885c68f355a0cbb0823d8188e387
ilmisan1995 <ilmisantiago3@gmail.com>
2026-04-20 23:06:11 +0700

    main.py

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ RED    = (255, 0, 0)
 GRAY   = (50, 50, 50)
 
 def get_font(size):
-    try: return pygame.font.Font("font_game.ttf", size)
-    except: return pygame.font.SysFont("Arial", size, bold=True)
+    try: return pygame.font.Font("fast-money-four.ttf", size)
+    except: return pygame.font.SysFont("Fast Money Four", size, bold=True)
 
 font_score = get_font(55)
 font_main = get_font(28)
 font_x = get_font(100)
-font_admin = pygame.font.SysFont("Arial", 16)
+font_admin = pygame.font.SysFont("Fast Money Four", 16)
 
 # --- LOAD SOUNDS ---
 def load_s(file):
     try: return pygame.mixer.Sound(file)
     except: return None
 
-s_correct = load_s("correct.mp3")
-s_wrong = load_s("wrong.mp3")
+s_correct = load_s("correct.wav")
+s_wrong = load_s("wrong.wav")
 s_fm_correct = load_s("fm_correct.wav")
 s_reveal = load_s("score_reveal.wav")
 s_buzzer = load_s("timer_end.wav")
@@ -82,8 +82,8 @@ while running:
         draw_t(f"RONDE {soal['ronde']} - {mult}X SCORE", font_main, YELLOW, 600, 35)
         draw_t(skor_t1, font_score, YELLOW, 150, 85); draw_t(skor_mid, font_score, YELLOW, 600, 85); draw_t(skor_t2, font_score, YELLOW, 1050, 85)
         for i in range(4):
-            draw_t("X", font_x, RED if i < salah1 else GRAY, 100+(i*70), 195)
-            draw_t("X", font_x, RED if i < salah2 else GRAY, 880+(i*70), 195)
+            draw_t("X", font_x, YELLOW if i < salah1 else GRAY, 100+(i*70), 195)
+            draw_t("X", font_x, YELLOW if i < salah2 else GRAY, 880+(i*70), 195)
         draw_t(soal["pertanyaan"], font_main, YELLOW, 600, 220)
         for i in range(8):
             y_p = 340 + (i * 45)
@@ -99,11 +99,11 @@ while running:
         fdb = data["fast_money"]
         for i in range(5):
             y_p = 180 + (i * 85)
-            t1 = fdb["P1"]["jwb"][i] if fm_st["b_j1"][i] else "---"
+            t1 = fdb["P1"]["jwb"][i] if fm_st["b_j1"][i] else "-----------"
             draw_t(f"{i+1}. {t1}", font_main, YELLOW, 120, y_p, False)
             s1 = fdb["P1"]["skr"][i] if fm_st["b_s1"][i] else "--"
             draw_t(s1, font_main, YELLOW, 520, y_p)
-            t2 = fdb["P2"]["jwb"][i] if fm_st["b_j2"][i] else "---"
+            t2 = fdb["P2"]["jwb"][i] if fm_st["b_j2"][i] else "-----------"
             draw_t(f"{i+1}. {t2}", font_main, YELLOW, 680, y_p, False)
             s2 = fdb["P2"]["skr"][i] if fm_st["b_s2"][i] else "--"
             draw_t(s2, font_main, YELLOW, 1080, y_p)
